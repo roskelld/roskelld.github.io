c7b0ec782cce21fb96ac2b2df8fb4a988cfd459e
Dean Roskell <dean@primevr.io>
2019-01-31 01:07:05 -0700

    In progress Games first pass Front page partial

--- js/site.js
+++ js/site.js
@@ -7,13 +7,22 @@ class WebSite {
         // Build DOM Elements
         this.gameCardGenerator( games, 'games' );
 
+        const content =  document.querySelector('#stuff')
+
+        // this.scroller = new SimpleBar( content, {});
         // .....................................................................
         // INITIALIZE MATERIALIZE
         const tabs = document.querySelectorAll( '.tabs' );
-        const tabOptions = {
+        this.tabOptions = {
+            // onShow: this.tabInit,
         };
+        this.tabInstances = M.Tabs.init( tabs, this.tabOptions );
+        setTimeout( () => {
+        }, 0);
 
-        this.tabInstances = M.Tabs.init( tabs, tabOptions );
+        const carousel = document.querySelectorAll( '.carousel' );
+        this.carouselOptions = {};
+        this.carouselInstances = M.Carousel.init( carousel, this.carouselOptions );
 
         const sideNav = document.querySelectorAll( '.sidenav' );
         this.sideNavInstances = M.Sidenav.init(sideNav, {} );
@@ -21,19 +30,19 @@ class WebSite {
         this.nav = {
             tabs:   {
                 home:           document.querySelector('#home'),
-                services:       document.querySelector('#services'),
+                // services:       document.querySelector('#services'),
                 games:          document.querySelector('#games'),
-                applications:   document.querySelector('#applications'),
-                projects:       document.querySelector('#projects'),
-                bio:            document.querySelector('#bio'),
+                // applications:   document.querySelector('#applications'),
+                // projects:       document.querySelector('#projects'),
+                // bio:            document.querySelector('#bio'),
             },
             mobile: {
                     home:           document.querySelector('#mobile-nav-home').addEventListener( 'click', () => { this.tabInstances[0].select('home'); this.sideNavInstances[0].close(); }, false ),
-                    services:       document.querySelector('#mobile-nav-services').addEventListener( 'click', () => { this.tabInstances[0].select('services');  this.sideNavInstances[0].close(); }, false ),
+                    // services:       document.querySelector('#mobile-nav-services').addEventListener( 'click', () => { this.tabInstances[0].select('services');  this.sideNavInstances[0].close(); }, false ),
                     games:          document.querySelector('#mobile-nav-games').addEventListener( 'click', () => { this.tabInstances[0].select('games'); this.sideNavInstances[0].close(); }, false ),
-                    applications:   document.querySelector('#mobile-nav-applications').addEventListener( 'click', () => { this.tabInstances[0].select('applications'); this.sideNavInstances[0].close(); }, false ),
-                    projects:       document.querySelector('#mobile-nav-projects').addEventListener( 'click', () => { this.tabInstances[0].select('projects'); this.sideNavInstances[0].close(); }, false ),
-                    bio:            document.querySelector('#mobile-nav-bio').addEventListener( 'click', () => { this.tabInstances[0].select('bio'); this.sideNavInstances[0].close(); }, false ),
+                    // applications:   document.querySelector('#mobile-nav-applications').addEventListener( 'click', () => { this.tabInstances[0].select('applications'); this.sideNavInstances[0].close(); }, false ),
+                    // projects:       document.querySelector('#mobile-nav-projects').addEventListener( 'click', () => { this.tabInstances[0].select('projects'); this.sideNavInstances[0].close(); }, false ),
+                    // bio:            document.querySelector('#mobile-nav-bio').addEventListener( 'click', () => { this.tabInstances[0].select('bio'); this.sideNavInstances[0].close(); }, false ),
             }
         }
 
@@ -48,7 +57,33 @@ class WebSite {
 
         this.cardClicks();
 
-        this.scroller = new SimpleBar( document.querySelector('main'), {});
+
+    }
+
+    tabInit( tab ) {
+        if ( typeof tab === 'undefined' ) tab = this.$activeTabLink[0].hash;
+        console.log( tab.id );
+
+        switch (tab.id) {
+            case 'home':
+
+                break;
+            case 'services':
+
+                break;
+            case 'games':
+                console.log('DING');
+                window.s.gameCardGenerator( games, 'games' );
+                break;
+            case 'applications':
+
+                break;
+            case 'projects':
+
+                break;
+            default:
+
+        }
     }
 
     gameCardGenerator( data, containerId ) {
@@ -69,7 +104,7 @@ class WebSite {
             const imgDiv    = document.createElement( 'div' );
             const img       = document.createElement( 'img' );
 
-            colDiv.classList.add( 'col', 's12', 'm3', 'l3' );
+            colDiv.classList.add( 'col', 's12', 'm12', 'l6', 'xl3' );
             cardDiv.classList.add( 'card', 'game-card' );
             cardDiv.id = `card-${item.id}`;
             imgDiv.classList.add( 'card-image' );
@@ -77,6 +112,15 @@ class WebSite {
             img.title = item.title;
 
             imgDiv.appendChild( img );
+
+            if ( item.release === 'N/A' ) {
+                const release = document.createElement( 'span' );
+                release.classList.add( 'unreleased' );
+                release.textContent = 'Unreleased';
+                imgDiv.appendChild( release );
+            }
+
+
             cardDiv.appendChild( imgDiv );
             colDiv.appendChild( cardDiv );
             links.appendChild( colDiv );
@@ -113,154 +157,80 @@ class WebSite {
             const detailDiv = document.createElement( 'div' );
             detailDiv.classList.add( 'col', 's12' );
             detailDiv.id = `content-${item.id}`;
-            // detailDiv.innerHTML = `
-            //     <div class="background-image content-section">
-            //     <div class="center-align">
-            //     <div class="section">
-            //     <h3>${item.title}</h3>
-            //     <p>${platforms}</p>
-            //     <p>${tech}</p>
-            //     <p>${item.release}</p>
-            //     <p>${item.company} / ${item.publisher}</p></div>
-            //     <div class="divider"></div>
-            //     <h5>${item.role}</h5>
-            //     <div class="tasks"></div>
-            //     </div>
-            //     <div class="divider"></div>
-            //     <div class="section">
-            //     <p class="detail-description">${item.description}</p>
-            //     </div>
-            //     </div>`;
 
+            // Trailer
+            // if ( typeof data.embed !== 'undefined' )
 
             detailDiv.innerHTML = `
-                <div class="">
-                    <div class="content-section">
-                        <div class="section"></div>
-
-                        <table>
-                        <tr>
-                            <td class="right-align detail-header"><span class="white black-text detail-header title">Title:</span></td>
-                            <td class="detail-data"><span class="detail-data title">${item.title}</span></td>
-                        </tr>
-                        </table>
-
-                        <div class="divider"></div>
-
-                        <table>
-                        <tr>
-                            <td class="right-align detail-header"><span class="white black-text detail-header">Platform(s):</span></td>
-                            <td class="detail-data"><span class="detail-data">${platforms}</span></td>
-                        </tr>
-                        <tr>
-                            <td class="right-align detail-header"><span class="white black-text detail-header">Genre:</span></td>
-                            <td class="detail-data"><span class="detail-data">${item.genre}</span></td>
-                        </tr>
-                        <tr>
-                            <td class="right-align"><span class="white black-text detail-header">Release:</span></td>
-                            <td class="detail-data"><span class="detail-data">${item.release}</span></td>
-                        </tr>
-                        <tr>
-                            <td class="right-align"><span class="white black-text detail-header">Company:</span></td>
-                            <td class="detail-data"><span class="detail-data">${item.company}</span></td>
-                        </tr>
-                        <tr>
-                            <td class="right-align"><span class="white black-text detail-header">Publisher:</span></td>
-                            <td class="detail-data"><span class="detail-data">${item.publisher}</span></td>
-                        </tr>
-                        </table>
-
-                        <div class="divider"></div>
-
-                        <table>
-                        <tr>
-                            <td class="right-align"><span class="white black-text detail-header">Technology:</span></td>
-                            <td class="detail-data"><span class="detail-data">${tech}</span></td>
-                        </tr>
-
-                        <tr>
-                            <td class="right-align"><span class="white black-text detail-header">Team Size:</span></td>
-                            <td class="detail-data"><span class="detail-data">${item.team_size}</span></td>
-                        </tr>
-
-                        <tr>
-                            <td class="right-align"><span class="white black-text detail-header">Dev Time:</span></td>
-                            <td class="detail-data"><span class="detail-data">${item.dev_time}</span></td>
-                        </tr>
-
-                        <tr>
-                            <td class="right-align"><span class="white black-text detail-header">Role:</span></td>
-                            <td class="detail-data"><span class="detail-data">${item.role}</span></td>
-                        </tr>
-                        <tr>
-                            <td class="right-align"><span class="white black-text detail-header">Responsibilities:</span></td>
-                            <td class="detail-data"><span class="detail-data">${tasks}</span></td>
-                        </tr>
-                        </table>
-                        <div class="divider"></div>
-                            <p class="detail-description">${item.description}</p>
+                <div class="section">
+                    <div class="row">
+                        <div class="col s3"><div class="white black-text detail-header title right">Title:</div></div>
+                        <div class="col s9"><div class="title">${item.title}</div></div>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="section">
+                        <div class="row">
+                            <div class="col s12 m3"><div class="white black-text detail-header right">Platform(s):</div></div>
+                            <div class="col s12 m9"><div class="detail-data">${platforms}</div></div>
+                        </div>
+                        <div class="row">
+                            <div class="col s3"><div class="white black-text detail-header right">Genre:</div></div>
+                            <div class="col s3"><div class="detail-data">${item.genre}</div></div>
+                            <div class="col s2"><div class="white black-text detail-header right">Company:</div></div>
+                            <div class="col s4"><div class="detail-data">${item.company}</div></div>
+                        </div>
+                        <div class="row">
+                            <div class="col s3"><div class="white black-text detail-header right">Release:</div></div>
+                            <div class="col s2"><div class="detail-data">${item.release}</div></div>
+                            <div class="col s3"><div class="white black-text detail-header right">Publisher:</div></div>
+                            <div class="col s4"><div class="detail-data">${item.publisher}</div></div>
+                        </div>
+                        <div class="row">
+                            <div class="col s3"><div class="white black-text detail-header right">Role:</div></div>
+                            <div class="col s9"><div class="detail-data">${item.role}</div></div>
+                        </div>
+                        <div class="row">
+                            <div class="col s3"><div class="white black-text detail-header right">Responsibilities:</div></div>
+                            <div class="col s9"><div class="detail-data">${tasks}</div></div>
+                        </div>
+                        <div class="row">
+                            <div class="col s3"><div class="white black-text detail-header right">Technology:</div></div>
+                            <div class="col s9"><div class="detail-data">${tech}</div></div>
+                        </div>
+                        <div class="row">
+                            <div class="col s3"><div class="white black-text detail-header right">Team Size:</div></div>
+                            <div class="col s9"><div class="detail-data">${item.team_size}</div></div>
+                        </div>
+                        <div class="row">
+                            <div class="col s3"><div class="white black-text detail-header right">Dev Time:</div></div>
+                            <div class="col s9"><div class="detail-data">${item.dev_time}</div></div>
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="section">
+                        <div class="row">
+                            <div class="col s12"><div class="center">${item.embed}</div></div>
                         </div>
                     </div>
                 </div>`;
 
             detail.appendChild( detailDiv );
-
-            // <div class="row">
-            // <div class="col s12 m2"><span class="white black-text detail-header">Title:</span></div>
-            // <div class="col s12 m10"><span class="detail-data">${item.title}</span></div>
-            // </div>
-            //
-            // <div class="row">
-            // <div class="col s12 m2"><span class="white black-text detail-header">Platform(s):</span></div>
-            // <div class="col s12 m10"><span class="detail-data">${platforms}</span></div>
-            // </div>
-            //
-            // <div class="row">
-            // <div class="col s12 m2"><span class="white black-text detail-header">Technology:</span></div>
-            // <div class="col s12 m10"><span class="detail-data">${tech}</span></div>
-            // </div>
-            //
-            // <div class="row">
-            // <div class="col s12 m2"><span class="white black-text detail-header">Release:</span></div>
-            // <div class="col s12 m10"><span class="detail-data">${item.release}</span></div>
-            // </div>
-            //
-            // <div class="row">
-            // <div class="col s12 m2"><span class="white black-text detail-header">Company:</span></div>
-            // <div class="col s12 m10"><span class="detail-data">${item.company}</span></div>
-            // </div>
-            //
-            // <div class="row">
-            // <div class="col s12 m2"><span class="white black-text detail-header">Publisher:</span></div>
-            // <div class="col s12 m10"><span class="detail-data">${item.publisher}</span></div>
-            // </div>
-            //
-            // <div class="row">
-            // <div class="col s12 m2"><span class="white black-text detail-header">Role:</span></div>
-            // <div class="col s12 m10"><span class="detail-data">${item.role}</span></div>
-            // </div>
-            //
-            // <div class="row">
-            // <div class="col s12 m2"><span class="white black-text detail-header">Responsibilities:</span></div>
-            // <div class="col s12 m10"><span class="detail-data">${tasks}</span></div>
-            // </div>
-
-            // <div class="section">
-            //     <p><span class="white black-text detail-header">Title:</span>       <span class="detail-data">${item.title}</span></p>
-            //     <p><span class="white black-text detail-header">Platform(s):</span> <span class="detail-data">${platforms}</span></p>
-            //     <p><span class="white black-text detail-header">Technology:</span>  <span class="detail-data">${tech}</span></p>
-            //     <p><span class="white black-text detail-header">Release:</span>     <span class="detail-data">${item.release}</span></p>
-            //     <p><span class="white black-text detail-header">Company:</span>     <span class="detail-data">${item.company}</span></p>
-            //     <p><span class="white black-text detail-header">Publisher:</span>   <span class="detail-data">${item.publisher}</span></p>
-            //     <p><span class="white black-text detail-header">Role:</span>        <span class="detail-data">${item.role}</span></p>
-            //     <p><span class="white black-text detail-header">Responsibilities:</span>  <span class="detail-data">${tasks}</span></p></p>
-            // </div>
-            //     <div class="divider"></div>
-            //     <h5>${item.role}</h5>
-
-            // detail.querySelector('.tasks').appendChild( tasks );
         });
 
+    }
+
+    digestDataGenerator( data, containerId ) {
+        // step through the data and build DOM elements, then add to container
+        const container = document.querySelector( `#${containerId}` );
+        const links = container.querySelector( '.contentlinks' );
+        const detail  = container.querySelector( '.contentdetail' );
+
+
+    }
+
+    updateScroller( tab ) {
+        console.log( window.s.scroller );
+        window.s.scroller.recalculate();
     }
 
     cardClicks() {
@@ -281,4 +251,5 @@ class WebSite {
 
 document.addEventListener( 'DOMContentLoaded', () => {
     const site = new WebSite();
+    window.s = site;
 }, false );
