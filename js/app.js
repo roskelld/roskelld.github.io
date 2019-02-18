3eef24c94b5e51a8f7fc374e8841a2c8825dcee7
Dean Roskell <dean@primevr.io>
2019-02-18 02:15:09 -0700

    testing

--- js/app.js
+++ js/app.js
@@ -692,10 +692,9 @@ class WebSite {
 
 const site = new WebSite();
 document.addEventListener( 'DOMContentLoaded', function() {
-    // const test = document.createElement( 'div' );
-    // test.innerHTML = `<h1 class="black-text">OTHER TESTING</h1>`;
-    // document.querySelector( 'header' ).appendChild( test );
-    document.querySelector( '#loader' ).classList.remove( 'loader' );
+    setTimeout( () => { 
+        document.querySelector( '#loader' ).classList.remove( 'loader' );
+    }, 10);
 }, false );
 
 
